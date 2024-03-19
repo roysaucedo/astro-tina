4356edc7f988c9ae38b53b9220b1af61dea80284
roysaucedo <roy.a.saucedo@gmail.com>
2024-03-18 21:33:05 -0700

    edits tina

--- frontend/tina/config.js
+++ frontend/tina/config.js
@@ -31,7 +31,7 @@ export default defineConfig({
       {
         name: "post",
         label: "Posts",
-        path: "content/posts",
+        path: "content/blog",
         fields: [
           {
             type: "string",
